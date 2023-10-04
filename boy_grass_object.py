023fe3e3f011c2411d7c92e033e2a9759f59d269
Tkadha <emforhs228@naver.com>
2023-10-04 19:04:47 +0900

    뛰어가는 캐릭터 여러개 만듦(수업 내용)

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,7 +1,32 @@
 from pico2d import *
+import random
 
 
 # Game object class here
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+    def update(self):
+        pass
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = random.randint(0, 7)
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
 
 def handle_events():
     global running
@@ -12,16 +37,33 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+
 def reset_world():
     global running
+    global grass
+    global team
+    global world
     running = True
+    world = []
+
+    grass = Grass()
+    world.append(grass)
+
+    team = [Boy() for i in range(11)]
+    world += team
+
 
 def update_world():
-    pass
+    for o in world:
+        o.update()
+
 
 def render_world():
     clear_canvas()
-    update_world()
+    for o in world:
+        o.draw()
+    update_canvas()
+
 
 open_canvas()
 
